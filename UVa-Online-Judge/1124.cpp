3da4e5912cf9f85622233bbf5884dae91b612c58
Máximo Leonel Di Gialleonardo <109329695+MaxiLDiG@users.noreply.github.com>
2023-05-07 15:06:50 -0300

    Ejercicios completados de UVa Online Judge

---
+++ UVa-Online-Judge/1124.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fore(i,a,n) for(ll i = a; i < int(n); i++)
+#define ALL(x) x.begin(),x.end()
+#define SZ(x) ((int)x.size())
+#define pb(n) push_back(n)
+#define INF 1000000007
+#define mp make_pair
+#define snd second
+#define fst first
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string a;
+	while(getline(cin,a)) cout<<a<<endl;
+
+	return 0;
+}
